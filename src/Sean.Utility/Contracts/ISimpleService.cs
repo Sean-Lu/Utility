93561e61e17e474aa6f5873199c6c38b8d7adf59
Lu Weixian <weixian.lu@foxmail.com>
2021-04-28 11:29:57 +0800

    增加ISimpleService

---
+++ src/Sean.Utility/Contracts/ISimpleService.cs
@@ -0,0 +1,20 @@
+﻿namespace Sean.Utility.Contracts
+{
+    public interface ISimpleService
+    {
+        void Start();
+        void Stop();
+    }
+
+    public interface ISimpleService<in T>
+    {
+        void Start(T data);
+        void Stop();
+    }
+
+    public interface ISimpleService<in T, in T2>
+    {
+        void Start(T data);
+        void Stop(T2 data);
+    }
+}
