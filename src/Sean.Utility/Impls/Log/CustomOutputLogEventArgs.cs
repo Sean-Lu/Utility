cea6e5555403b138a613da72e1874559dfcf0f5f
Lu Weixian <weixian.lu@foxmail.com>
2021-05-25 22:29:24 +0800

    添加被自动忽略的文件

---
+++ src/Sean.Utility/Impls/Log/CustomOutputLogEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using Sean.Utility.Enums;
+using System;
+using Sean.Utility.Contracts;
+
+namespace Sean.Utility.Impls.Log
+{
+    public class CustomOutputLogEventArgs : EventArgs
+    {
+        public CustomOutputLogEventArgs(ISimpleLocalLoggerOptions options, LogLevel logLevel, string message, Exception exception)
+        {
+            Options = options;
+            LogLevel = logLevel;
+            Message = message;
+            Exception = exception;
+        }
+
+        public ISimpleLocalLoggerOptions Options { get; }
+        public LogLevel LogLevel { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
---
+++ src/Sean.Utility/Impls/Log/MsgFormatEventArgs.cs
@@ -0,0 +1,35 @@
+﻿using Sean.Utility.Enums;
+using System;
+using Sean.Utility.Contracts;
+
+namespace Sean.Utility.Impls.Log
+{
+    public class MsgFormatEventArgs : EventArgs
+    {
+        public MsgFormatEventArgs(ISimpleLocalLoggerOptions options, Type classType, LogLevel logLevel, string message, Exception exception)
+        {
+            Options = options;
+            ClassType = classType;
+            LogLevel = logLevel;
+            Message = message;
+            Exception = exception;
+
+            MsgFormat = message;
+        }
+
+        public ISimpleLocalLoggerOptions Options { get; }
+        public Type ClassType { get; }
+        public LogLevel LogLevel { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+
+        /// <summary>
+        /// 格式化后的消息
+        /// </summary>
+        public string MsgFormat { get; set; }
+        /// <summary>
+        /// 是否已经将信息输出到<see cref="Console"/>控制台窗口
+        /// </summary>
+        public bool HasOutputConsole { get; set; }
+    }
+}
