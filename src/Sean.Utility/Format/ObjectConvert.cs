8e7610df5466a38a9d8c73eb2f9f871e667f7523
Lu Weixian <luwx@newgrand.cn>
2021-12-24 14:05:46 +0800

    ObjectConvert

--- src/Sean.Utility/Format/ObjectConvert.cs
+++ src/Sean.Utility/Format/ObjectConvert.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Sean.Utility.Serialize;
+using System.Collections.Generic;
+using Sean.Utility.Extensions;
 
 namespace Sean.Utility.Format
 {
@@ -8,84 +9,119 @@ namespace Sean.Utility.Format
     /// </summary>
     public class ObjectConvert
     {
+        #region 对象映射
         /// <summary>
-        /// 将object对象转换为实体对象（反射）
+        /// 对象映射
         /// </summary>
-        /// <typeparam name="T">实体对象类名</typeparam>
-        /// <param name="source">object对象</param>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
         /// <returns></returns>
-        public static T ConvertObject<T>(object source) where T : class, new()
+        public static TDestination Map<TDestination, TSource>(TSource source) where TDestination : new()
         {
-            //创建实体对象实例
-            var result = Activator.CreateInstance<T>();
-            return ConvertObject(source, result);
+            var result = Activator.CreateInstance<TDestination>();
+            Map(result, source);
+            return result;
         }
         /// <summary>
-        /// 将object对象转换为实体对象（反射）
+        /// 对象映射
         /// </summary>
-        /// <typeparam name="T">实体对象类名</typeparam>
-        /// <param name="source">object对象</param>
-        /// <param name="outputObject">object对象</param>
-        /// <returns></returns>
-        public static T ConvertObject<T>(object source, T outputObject) where T : class
+        /// <typeparam name="TDestination"></typeparam>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="destination"></param>
+        /// <param name="source"></param>
+        public static void Map<TDestination, TSource>(TDestination destination, TSource source)
         {
-            if (outputObject == null)
+            if (source == null || destination == null)
+            {
+                return;
+            }
+
+            var typeSource = typeof(TSource);
+            var typeDestination = typeof(TDestination);
+            foreach (var destinationPropertyInfo in typeDestination.GetProperties())
+            {
+                var sourcePropertyInfo = typeSource.GetProperty(destinationPropertyInfo.Name);
+                if (sourcePropertyInfo != null && destinationPropertyInfo.GetSetMethod() != null && sourcePropertyInfo.GetGetMethod() != null)
+                {
+                    destinationPropertyInfo.SetValue(destination, sourcePropertyInfo.GetValue(source, null), null);
+                }
+            }
+        }
+        /// <summary>
+        /// 对象映射
+        /// </summary>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static List<TDestination> MapList<TDestination, TSource>(List<TSource> source) where TDestination : new()
+        {
+            if (source == null)
             {
                 return null;
             }
 
-            if (source != null)
+            var result = new List<TDestination>();
+            source.ForEach(c =>
             {
-                Type type = source.GetType();
-                var properties = typeof(T).GetProperties();
-                //遍历实体对象属性
-                foreach (var info in properties)
-                {
-                    var propertyInfo = type.GetProperty(info.Name);
-                    if (propertyInfo == null || !propertyInfo.CanWrite)
-                    {
-                        continue;
-                    }
-
-                    //取得object对象中此属性的值
-                    var val = propertyInfo.GetValue(source, null);
-                    if (val != null)
-                    {
-                        object obj;
-                        if (!info.PropertyType.IsGenericType)
-                        {
-                            //非泛型
-                            obj = Convert.ChangeType(val, info.PropertyType);
-                        }
-                        else
-                        {
-                            //泛型Nullable<>
-                            Type genericTypeDefinition = info.PropertyType.GetGenericTypeDefinition();
-                            if (genericTypeDefinition == typeof(Nullable<>))
-                            {
-                                obj = Convert.ChangeType(val, Nullable.GetUnderlyingType(info.PropertyType));
-                            }
-                            else
-                            {
-                                obj = Convert.ChangeType(val, info.PropertyType);
-                            }
-                        }
-                        info.SetValue(outputObject, obj, null);
-                    }
-                }
-            }
-            return outputObject;
+                var model = Activator.CreateInstance<TDestination>();
+                Map(model, c);
+                result.Add(model);
+            });
+            return result;
         }
+        #endregion
+
+        #region 类型转换
         /// <summary>
-        /// 将object对象转换为实体对象（json序列化\反序列化）
+        /// 类型转换
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
+        /// <param name="obj"></param>
         /// <returns></returns>
-        public static T ConvertObjectByJson<T>(object source)
+        public static T ChangeType<T>(object obj)
         {
-            var json = JsonHelper.Serialize(source);
-            return JsonHelper.Deserialize<T>(json);
+            return (T)ChangeType(obj, typeof(T));
         }
+        /// <summary>
+        /// 类型转换
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object ChangeType(object obj, Type type)
+        {
+            // √ 字符串类型 转 DateTime类型
+            // √ 数值类型 转 bool类型
+            // √ 数值类型 转 枚举类型
+
+            if (obj == null || obj == DBNull.Value)
+            {
+                return type.GetDefaultValue();
+            }
+
+            var objType = obj.GetType();
+            if (objType == type)
+            {
+                return obj;
+            }
+
+            if (type.IsEnum)
+            {
+                return Enum.ToObject(type, obj);
+            }
+
+            var changeType = Nullable.GetUnderlyingType(type) ?? type;
+            if (Equals(obj, string.Empty) && changeType == typeof(DateTime))
+            {
+                return type.GetDefaultValue();
+            }
+
+            //var result = Convert.ChangeType(obj, type);// 可空类型会报错
+            var result = Convert.ChangeType(obj, changeType);
+            return result;
+        }
+        #endregion
     }
 }
